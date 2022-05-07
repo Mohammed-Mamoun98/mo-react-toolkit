9a0a1693d51c2d7e807a9c219cdf9711c6c39364
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2022-05-07 21:09:40 +0300

    add getContractAddress and findInContracts methods

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,30 +1,63 @@
+const findInContracts = (contractName) => {
+  let newContractsObject = {};
+
+  Object.entries(contracts).forEach(([key, value]) => {
+    const newKey = key.toLocaleLowerCase();
+    newContractsObject = { ...newContractsObject, [newKey]: value };
+  });
+
+  const lowerCasedName = contractName.toLocaleLowerCase();
+  return newContractsObject?.[lowerCasedName];
+};
+
+export const getContractAddress = async (
+  contractName,
+  network = configJson.network
+) => {
+  if (!window.contracts) {
+    await wait(150);
+    return getContractAddress(contractName);
+  }
+  const contract = findInContracts(contractName);
+  const contractAddress = getValueByMultiProps(contract?.networks?.[network], [
+    "Proxy",
+    "address",
+  ]);
+  return contractAddress;
+};
 
 export const contractReader = async ({
-    contractName,
-    functionName = '',
-    params = [],
-    contractAddress: _contractAddress, // could be passed in cases like HPool contract
-    sendParams, // like from and value that are passed to send function
-    onTxHash = () => {}, // could be extended to events param in Future
-  }) =>
-    new Promise(async (resolve, reject) => {
-      await loadContracts();
-      const contractAddress = _contractAddress || (await getContractAddress(contractName));
-      const _contract = await createContract(contractName, contractAddress, contracts);
-      const contractInstance = await _contract();
-      try {
-        if (!sendParams) {
-          const result = await contractInstance.methods?.[functionName]?.(...params).call();
-          resolve(result);
-        } else {
-          contractInstance.methods?.[functionName]?.(...params)
-            .send({ ...sendParams })
-            .once('receipt', (rec) => resolve(rec))
-            .once('transactionHash', onTxHash)
-            .on('error', (err) => reject(err));
-        }
-      } catch (error) {
-        reject(error.message);
+  contractName,
+  functionName = "",
+  params = [],
+  contractAddress: _contractAddress, // could be passed in cases like HPool contract
+  sendParams, // like from and value that are passed to send function
+  onTxHash = () => {}, // could be extended to events param in Future
+}) =>
+  new Promise(async (resolve, reject) => {
+    await loadContracts();
+    const contractAddress =
+      _contractAddress || (await getContractAddress(contractName));
+    const _contract = await createContract(
+      contractName,
+      contractAddress,
+      contracts
+    );
+    const contractInstance = await _contract();
+    try {
+      if (!sendParams) {
+        const result = await contractInstance.methods?.[functionName]?.(
+          ...params
+        ).call();
+        resolve(result);
+      } else {
+        contractInstance.methods?.[functionName]?.(...params)
+          .send({ ...sendParams })
+          .once("receipt", (rec) => resolve(rec))
+          .once("transactionHash", onTxHash)
+          .on("error", (err) => reject(err));
       }
-    });
-  
+    } catch (error) {
+      reject(error.message);
+    }
+  });
